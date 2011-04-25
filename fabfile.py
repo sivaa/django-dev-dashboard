9947907ac5f526ffcb95d9e82a5c1c03bc5cbd6c
Jacob Kaplan-Moss <jacob@jacobian.org>
2011-04-25 10:19:58 -0500

    Added a quick fab command to sync data locally.

--- fabfile.py
+++ fabfile.py
@@ -3,3 +3,7 @@ from fabric.api import *
 def deploy():
     local('./manage.py collectstatic --noinput')
     local('epio upload')
+
+def copydb():
+    local('epio django -- dumpdata --natural dashboard > dddash.json')
+    local('./manage.py loaddata dddash.json')
