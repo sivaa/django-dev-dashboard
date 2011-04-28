7e1552712fd1781d5275256a18047ca409ead6f6
Jacob Kaplan-Moss <jacob@jacobian.org>
2011-04-28 15:47:45 -0500

    Add last-modified headers to metric JSON.

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,8 +5,9 @@ from django import http
 from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.cache import cache_page
+from django.views.decorators.http import last_modified
 from django.utils import simplejson
-from .models import TracTicketMetric
+from .models import TracTicketMetric, Datum
 
 @cache_page(60 * 10)
 def index(request):
@@ -17,7 +18,14 @@ def index(request):
         data.append({'metric': metric, 'latest': latest,})
     return render(request, 'dashboard/index.html', {'data': data})
 
+def metric_last_modified(request, metric_id):
+    try:
+        return Datum.objects.filter(object_id=metric_id).latest().timestamp
+    except Datum.DoesNotExist:
+        return None
+
 @cache_page(60 * 10)
+@last_modified(metric_last_modified)
 def metric_json(request, metric_id):
     metric = get_object_or_404(TracTicketMetric, pk=metric_id)
     
