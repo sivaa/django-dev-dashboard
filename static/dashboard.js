76c4de46af8d71f54738168e1c53a29d2e3dc7e5
Jacob Kaplan-Moss <jacob@jacobian.org>
2011-04-28 16:13:05 -0500

    Show timestamps upon hovering.

--- static/dashboard.js
+++ static/dashboard.js
@@ -2,10 +2,16 @@ $(function () {
     $("div.metric div.sparkline").each(function (index, elem) {
         var e = $(elem);
         var value_element = e.parent().find('p.value a');
+        var timestamp_element = e.parent().find('span.timestamp');
         var original_value = value_element.html();
         
         var url = "/metric/" + e.data('metric') + ".json";
         $.getJSON(url, function(data) {
+            // flot time series data needs to be in *milliseconds*, not seconds.
+            // fixing this in Python would be easier but would limit reuse.
+            for (var i=0; i < data.length; i++) {
+                data[i][0] = data[i][0] * 1000;
+            };
             var options = {
                 xaxis: {show: false, mode: "time"},
                 yaxis: {show: false, min: 0},
@@ -16,8 +22,11 @@ $(function () {
             e.bind('plothover', function(event, pos, item) {
                 if (item) {
                     value_element.html(item.datapoint[1]);
+                    var d = new Date(item.datapoint[0]);
+                    timestamp_element.html($.plot.formatDate(d, "%b %d, %h:%M%p"));
                 } else {
                     value_element.html(original_value);
+                    timestamp_element.html('&nbsp;');
                 }
             });
         });
