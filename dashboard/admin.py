69729dc7a2ba0bd6ebbeb768856774cbd87cb9f9
Jacob Kaplan-Moss <jacob@jacobian.org>
2011-04-29 17:08:02 -0500

    Made the admin and management command ABC-worthy.

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,13 +1,15 @@
 from __future__ import absolute_import
 
 from django.contrib import admin
-from .models import TracTicketMetric, Datum
+from .models import Metric, Datum
 
-admin.site.register(TracTicketMetric, 
-    list_display = ('name', 'query', 'show_on_dashboard', 'show_sparkline'),
-    list_editable = ('show_on_dashboard', 'show_sparkline'),
-    prepopulated_fields = {'slug': ['name']},
-)
+class MetricAdmin(admin.ModelAdmin):
+    list_display = ('name', 'show_on_dashboard', 'show_sparkline')
+    list_editable = ('show_on_dashboard', 'show_sparkline')
+    prepopulated_fields = {'slug': ['name']}
+
+for MC in Metric.__subclasses__():
+    admin.site.register(MC, MetricAdmin)
 
 admin.site.register(Datum, 
     list_display = ('timestamp', 'metric', 'measurement'),
--- dashboard/management/commands/update_metrics.py
+++ dashboard/management/commands/update_metrics.py
@@ -1,15 +1,16 @@
 from __future__ import absolute_import, print_function
 
 from django.core.management.base import NoArgsCommand
-from ...models import TracTicketMetric
+from ...models import Metric
 
 class Command(NoArgsCommand):
     
     def handle_noargs(self, **options):
         verbose = int(options.get('verbosity', 0))
-        for metric in TracTicketMetric.objects.all():
-            if verbose:
-                print("Updating %s ... " % metric.name.lower(), end="")
-            datum = metric.data.create(measurement=metric.fetch())
-            if verbose:
-                print(datum.measurement)
+        for MC in Metric.__subclasses__():
+            for metric in MC.objects.all():
+                if verbose:
+                    print("Updating %s ... " % metric.name.lower(), end="")
+                datum = metric.data.create(measurement=metric.fetch())
+                if verbose:
+                    print(datum.measurement)
