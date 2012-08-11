d90aefcdc59d5c2b83b01eda209e118d4a888dcb
Ramiro Morales <ramiro@rmorales.net>
2012-08-10 23:15:59 -0300

    Add boolean switches, doctrings. Refactor data fetching.

--- dashboard/migrations/0009_add_jenkinsfailuresmetric.py
+++ dashboard/migrations/0009_add_jenkinsfailuresmetric.py
@@ -8,8 +8,8 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'JenkinsMetric'
-        db.create_table('dashboard_jenkinsmetric', (
+        # Adding model 'JenkinsFailuresMetric'
+        db.create_table('dashboard_jenkinsfailuresmetric', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=300)),
             ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50)),
@@ -18,14 +18,16 @@ class Migration(SchemaMigration):
             ('period', self.gf('django.db.models.fields.CharField')(default='instant', max_length=15)),
             ('unit', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('unit_plural', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('jenkins_instance_root_url', self.gf('django.db.models.fields.URLField')(max_length=1000)),
-            ('build', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('jenkins_root_url', self.gf('django.db.models.fields.URLField')(max_length=1000)),
+            ('build_name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('is_success_cnt', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('is_percentage', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
-        db.send_create_signal('dashboard', ['JenkinsMetric'])
+        db.send_create_signal('dashboard', ['JenkinsFailuresMetric'])
 
     def backwards(self, orm):
-        # Deleting model 'JenkinsMetric'
-        db.delete_table('dashboard_jenkinsmetric')
+        # Deleting model 'JenkinsFailuresMetric'
+        db.delete_table('dashboard_jenkinsfailuresmetric')
 
     models = {
         'contenttypes.contenttype': {
@@ -56,11 +58,13 @@ class Migration(SchemaMigration):
             'unit': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'unit_plural': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        'dashboard.jenkinsmetric': {
-            'Meta': {'object_name': 'JenkinsMetric'},
-            'build': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+        'dashboard.jenkinsfailuresmetric': {
+            'Meta': {'object_name': 'JenkinsFailuresMetric'},
+            'build_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'jenkins_instance_root_url': ('django.db.models.fields.URLField', [], {'max_length': '1000'}),
+            'is_percentage': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_success_cnt': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'jenkins_root_url': ('django.db.models.fields.URLField', [], {'max_length': '1000'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '300'}),
             'period': ('django.db.models.fields.CharField', [], {'default': "'instant'", 'max_length': '15'}),
             'show_on_dashboard': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
--- dashboard/models.py
+++ dashboard/models.py
@@ -24,25 +24,25 @@ class Metric(models.Model):
     data = GenericRelation('Datum')
     show_on_dashboard = models.BooleanField(default=True)
     show_sparkline = models.BooleanField(default=True)
-    period = models.CharField(max_length=15, choices=METRIC_PERIOD_CHOICES, 
+    period = models.CharField(max_length=15, choices=METRIC_PERIOD_CHOICES,
                               default=METRIC_PERIOD_INSTANT)
     unit = models.CharField(max_length=100)
     unit_plural = models.CharField(max_length=100)
 
     class Meta:
         abstract = True
-        
+
     def __unicode__(self):
         return self.name
-        
+
     @models.permalink
     def get_absolute_url(self):
         return ("metric-detail", [self.slug])
-        
+
     def gather_data(self, since):
         """
         Gather all the data from this metric since a given date.
-        
+
         Returns a list of (timestamp, value) tuples. The timestamp is a Unix
         timestamp, coverted from localtime to UTC.
         """
@@ -54,11 +54,11 @@ class Metric(models.Model):
             return self._gather_data_periodic(since, 'week')
         else:
             raise ValueError("Unknown period: %s", self.period)
-    
+
     def _gather_data_instant(self, since):
         """
         Gather data from an "instant" metric.
-        
+
         Instant metrics change every time we measure them, so they're easy:
         just return every single measurement.
         """
@@ -70,23 +70,23 @@ class Metric(models.Model):
     def _gather_data_periodic(self, since, period):
         """
         Gather data from "periodic" merics.
-        
+
         Period metrics are reset every day/week/month and count up as the period
         goes on. Think "commits today" or "new tickets this week".
-        
+
         XXX I'm not completely sure how to deal with this since time zones wreak
         havoc, so there's right now a hard-coded offset which doesn't really
         scale but works for now.
         """
         OFFSET = "2 hours" # HACK!
         ctid = ContentType.objects.get_for_model(self).id
-        
+
         c = connections['default'].cursor()
-        c.execute('''SELECT 
+        c.execute('''SELECT
                         DATE_TRUNC(%s, timestamp - INTERVAL %s),
                         MAX(measurement)
                      FROM dashboard_datum
-                     WHERE content_type_id = %s 
+                     WHERE content_type_id = %s
                        AND object_id = %s
                        AND timestamp >= %s
                      GROUP BY 1;''', [period, OFFSET, ctid, self.id, since])
@@ -94,24 +94,24 @@ class Metric(models.Model):
 
 class TracTicketMetric(Metric):
     query = models.TextField()
-    
+
     def __unicode__(self):
         return self.name
-    
+
     def fetch(self):
         s = xmlrpclib.ServerProxy(settings.TRAC_RPC_URL)
         return len(s.ticket.query(self.query + "&max=0"))
-    
+
     def link(self):
         return "%squery?%s" % (settings.TRAC_URL, self.query)
 
 class RSSFeedMetric(Metric):
     feed_url = models.URLField(max_length=1000)
     link_url = models.URLField(max_length=1000)
-    
+
     def fetch(self):
         return len(feedparser.parse(self.feed_url).entries)
-    
+
     def link(self):
         return self.link_url
 
@@ -137,18 +137,41 @@ class GithubItemCountMetric(Metric):
             if c < 100:
                 break
         return count
-        
+
     def link(self):
         return self.link_url
 
-class JenkinsMetric(Metric):
-    jenkins_instance_root_url = models.URLField(max_length=1000)
-    build = models.CharField(max_length=100)
+class JenkinsFailuresMetric(Metric):
+    """
+    Track failures of a job/build. Uses the Python flavor of the Jenkins REST
+    API.
+    """
+    jenkins_root_url = models.URLField(
+        verbose_name='Jenkins instance root URL',
+        max_length=1000,
+        help_text='E.g. http://ci.djangoproject.com/',
+    )
+    build_name = models.CharField(
+        max_length=100,
+        help_text='E.g. Django Python3',
+    )
+    is_success_cnt = models.BooleanField(
+        verbose_name='Should the metric be a value representing success ratio?',
+        help_text='E.g. if there are 50 tests of which 30 are failing the value of this metric will be 20 (or 40%.)',
+    )
+    is_percentage = models.BooleanField(
+        verbose_name='Should the metric be a percentage value?',
+        help_text='E.g. if there are 50 tests of which 30 are failing the value of this metric will be 60%.',
+    )
 
     def urljoin(self, *parts):
         return '/'.join(p.strip('/') for p in parts)
 
-    def fetch(self):
+    def _fetch(self):
+        """
+        Actually get the values we are interested in by using the Jenkins REST
+        API (https://wiki.jenkins-ci.org/display/JENKINS/Remote+access+API)
+        """
         api_url = self.urljoin(self.link(), 'api/python')
         job_desc = requests.get(api_url)
         job_dict = ast.literal_eval(job_desc.text)
@@ -156,14 +179,26 @@ class JenkinsMetric(Metric):
         build_url = self.urljoin(build_ptr_dict['url'], 'api/python')
         build_desc = requests.get(build_url)
         build_dict = ast.literal_eval(build_desc.text)
-        fail_count = build_dict['actions'][4]['failCount']
-        total_count = build_dict['actions'][4]['totalCount']
-        if not total_count:
-            return 0
-        return (fail_count * 100)/total_count
+        return (build_dict['actions'][4]['failCount'], build_dict['actions'][4]['totalCount'])
+
+    def _calculate(self, failures, total):
+        """Calculate the metric value."""
+        if self.is_success_cnt:
+            value = total - failures
+        else:
+            value = failures
+        if self.is_percentage:
+            if not total:
+                return 0
+            value = (value * 100)/total
+        return value
+
+    def fetch(self):
+        failures, total = self._fetch()
+        return self._calculate(failures, total)
 
     def link(self):
-        return self.urljoin(self.jenkins_instance_root_url, 'job', self.build)
+        return self.urljoin(self.jenkins_root_url, 'job', self.build_name)
 
 class Datum(models.Model):
     metric = GenericForeignKey()
@@ -171,11 +206,11 @@ class Datum(models.Model):
     object_id = models.PositiveIntegerField()
     timestamp = models.DateTimeField(default=datetime.datetime.now)
     measurement = models.BigIntegerField()
-    
+
     class Meta:
         ordering = ['-timestamp']
         get_latest_by = 'timestamp'
         verbose_name_plural = 'data'
-        
+
     def __unicode__(self):
         return "%s at %s: %s" % (self.metric, self.timestamp, self.measurement)
